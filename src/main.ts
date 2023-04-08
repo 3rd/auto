6d01e5c01f8395059ef9b5460dd9ddd8ad35e8ab
3rd <3rd@users.noreply.github.com>
2023-04-08 23:15:25 +0300

    feat: configurable repository path (~/.config/auto and env)

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import repl from "node:repl";
-import chalk from "chalk";
 
+import envPaths from "env-paths";
+import chalk from "chalk";
 import spawn from "cross-spawn";
 import { cli as cleye, command } from "cleye";
 import { globSync } from "glob";
@@ -15,94 +16,75 @@ import Context from "./context/Context";
 import { AsTemplateType } from "./types";
 
 const createListCommand = (context: Context, templates: ReturnType<AsTemplateType>[]) =>
-  command(
-    {
-      name: "list",
-      alias: "ls",
-      flags: {
-        all: Boolean,
-      },
-    },
-    (argv) => {
-      const filteredTemplates = argv.flags.all
-        ? templates
-        : templates.filter((t) => !t.isValidForContext || t.isValidForContext(context));
-      for (const template of filteredTemplates) {
-        console.log(`[${template.id}] ${template.title}`);
-      }
+  command({ name: "list", alias: "ls", flags: { all: Boolean } }, (argv) => {
+    const filteredTemplates = argv.flags.all
+      ? templates
+      : templates.filter((t) => !t.isValidForContext || t.isValidForContext(context));
+    for (const template of filteredTemplates) {
+      console.log(`[${template.id}] ${template.title}`);
     }
-  );
+  });
 
 const createRunCommand = (context: Context, templates: ReturnType<AsTemplateType>[]) =>
-  command(
-    {
-      name: "run",
-      alias: "r",
-      parameters: ["<generator id>"],
-      flags: {
-        dry: Boolean,
-      },
-    },
-    async (argv) => {
-      const { generatorId } = argv._;
-      const template = templates.find((t) => t.id === generatorId);
-      if (!template) {
-        console.error(chalk.red(`Error: Template "%s" not found.`), generatorId);
-        process.exit(1);
-      }
-      if (template.isValidForContext && !template.isValidForContext(context)) {
-        console.error(chalk.red(`Error: Template "%s" is not valid for this context.`), generatorId);
-        process.exit(1);
-      }
-      const templateParams = template.bootstrapParams();
-      for (const [_, param] of Object.entries(templateParams)) {
-        const type = param.type as unknown;
-        if (type === "boolean") {
-          const { value } = await enquirer.prompt<{ value: string }>({
-            type: "confirm",
-            name: "value",
-            message: param.title,
-            initial: param.value,
-          });
-          param.value = value;
-        }
-        if (type === "number") {
-          const { value } = await enquirer.prompt<{ value: string }>({
-            type: "numeral",
-            name: "value",
-            message: param.title,
-            initial: param.value,
-          });
-          param.value = value;
-        }
-        if (type === "string") {
-          const { value } = await enquirer.prompt<{ value: string }>({
-            type: "input",
-            name: "value",
-            message: param.title,
-            initial: param.value,
-          });
-          param.value = value;
-        }
-      }
-
-      const paramValues = Object.fromEntries(Object.entries(templateParams).map(([key, param]) => [key, param.value]));
-
-      console.log(chalk.magenta(`Running "%s"...`), template.id);
-      template.generate({
-        context,
-        self: template,
-        params: paramValues as Parameters<typeof template.generate>[0]["params"],
-        substitute: (source, params: Partial<Parameters<typeof template.generate>[0]["params"]>) => {
-          let result = source;
-          for (const [key, value] of Object.entries(params)) {
-            result = result.replace(new RegExp(`{{${key}}}`, "g"), value as string);
-          }
-          return result;
-        },
-      });
+  command({ name: "run", alias: "r", parameters: ["<generator id>"] }, async (argv) => {
+    const { generatorId } = argv._;
+    const template = templates.find((t) => t.id === generatorId);
+    if (!template) {
+      console.error(chalk.red(`Error: Template "%s" not found.`), generatorId);
+      process.exit(1);
     }
-  );
+    if (template.isValidForContext && !template.isValidForContext(context)) {
+      console.error(chalk.red(`Error: Template "%s" is not valid for this context.`), generatorId);
+      process.exit(1);
+    }
+    const templateParams = template.bootstrapParams();
+    for (const [_, param] of Object.entries(templateParams)) {
+      const type = param.type as unknown;
+      if (type === "boolean") {
+        const { value } = await enquirer.prompt<{ value: string }>({
+          type: "confirm",
+          name: "value",
+          message: param.title,
+          initial: param.value,
+        });
+        param.value = value;
+      }
+      if (type === "number") {
+        const { value } = await enquirer.prompt<{ value: string }>({
+          type: "numeral",
+          name: "value",
+          message: param.title,
+          initial: param.value,
+        });
+        param.value = value;
+      }
+      if (type === "string") {
+        const { value } = await enquirer.prompt<{ value: string }>({
+          type: "input",
+          name: "value",
+          message: param.title,
+          initial: param.value,
+        });
+        param.value = value;
+      }
+    }
+
+    const paramValues = Object.fromEntries(Object.entries(templateParams).map(([key, param]) => [key, param.value]));
+
+    console.log(chalk.magenta(`Running "%s"...`), template.id);
+    template.generate({
+      context,
+      self: template,
+      params: paramValues as Parameters<typeof template.generate>[0]["params"],
+      substitute: (source, params: Partial<Parameters<typeof template.generate>[0]["params"]>) => {
+        let result = source;
+        for (const [key, value] of Object.entries(params)) {
+          result = result.replace(new RegExp(`{{${key}}}`, "g"), value as string);
+        }
+        return result;
+      },
+    });
+  });
 
 const createReplCommand = (context: Context, templates: ReturnType<AsTemplateType>[]) =>
   command({ name: "repl" }, async () => {
@@ -115,16 +97,36 @@ const createReplCommand = (context: Context, templates: ReturnType<AsTemplateTyp
   });
 
 const main = async () => {
-  // const templateRepositoryPath = resolve(dirname(fileURLToPath(import.meta.url)), "..", "templates");
-  const templateRepositoryPath = "/home/rabbit/brain/core/test/templates";
-  // const templateRepositoryPath = "/home/rabbit/brain/core/generator/templates";
+  const developmentRepositoryPath = resolve(dirname(fileURLToPath(import.meta.url)), "..", "templates");
+  const configRepositoryPath = envPaths(packageJson.name, { suffix: "" }).config;
+  const envRepositoryPath = process.env.REPO;
+  const repositoryPath = fs.existsSync(developmentRepositoryPath)
+    ? developmentRepositoryPath
+    : envRepositoryPath ?? configRepositoryPath;
+
+  if (!fs.existsSync(repositoryPath)) {
+    console.error(chalk.red("Error:"), "Cannot resolve repository directory, to fix this either:");
+    console.log(`- Create a directory at: ${chalk.magenta(configRepositoryPath)}`);
+    console.log(`- Or set the ${chalk.cyan("$REPO")} environment variable.`);
+
+    // auto-create repo directory
+    const { value: ok } = await enquirer.prompt<{ value: string }>({
+      type: "confirm",
+      name: "value",
+      message: `Do you want me to create a directory at ${chalk.magenta(configRepositoryPath)}?`,
+    });
+    if (ok) {
+      await fs.mkdirp(configRepositoryPath);
+      console.log(chalk.green("Success:"), "Created directory at", chalk.magenta(configRepositoryPath));
+    }
+  }
 
   if (typeof process.send !== "function") {
     const argv = process.argv.slice(1);
     const esmLoaderPath = require.resolve("tsx");
 
     // auto-setup repo/tsconfig.json
-    const tsconfigPath = resolve(templateRepositoryPath, "tsconfig.json");
+    const tsconfigPath = resolve(repositoryPath, "tsconfig.json");
     if (!fs.existsSync(tsconfigPath)) {
       console.log(chalk.yellow.bold("Warning:"), "Cannot find", chalk.cyan("tsconfig.json"), "in your repository.");
 
@@ -133,7 +135,6 @@ const main = async () => {
         name: "value",
         message: "Do you want me to set it up?",
       });
-
       if (ok) {
         const pathToDist = resolve(dirname(fileURLToPath(import.meta.url)), "..", "dist");
         await fs.writeFile(
@@ -166,7 +167,7 @@ const main = async () => {
   const context = new Context();
 
   const templates: ReturnType<AsTemplateType>[] = [];
-  for (const file of globSync(`${templateRepositoryPath}/**/*.ts`)) {
+  for (const file of globSync(`${repositoryPath}/**/*.ts`)) {
     const template = await import(file);
     templates.push(template.default);
   }
