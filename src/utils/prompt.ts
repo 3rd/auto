3730f28b78c5ddf29bcb738968bd5f0032ee1731
3rd <3rd@users.noreply.github.com>
2023-05-25 08:39:39 +0300

    chore: prepare prompts for extension and add semantic-release

--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -1,7 +1,7 @@
-import enquirer from "enquirer";
+import { prompt, Prompt } from "enquirer";
 
 export const confirm = async (message: string, initialValue?: boolean) => {
-  const { value } = await enquirer.prompt<{ value: boolean }>({
+  const { value } = await prompt<{ value: boolean }>({
     type: "confirm",
     name: "value",
     message,
@@ -11,7 +11,7 @@ export const confirm = async (message: string, initialValue?: boolean) => {
 };
 
 export const string = async (message: string, initialValue?: string) => {
-  const { value } = await enquirer.prompt<{ value: string }>({
+  const { value } = await prompt<{ value: string }>({
     type: "input",
     name: "value",
     message,
@@ -21,7 +21,7 @@ export const string = async (message: string, initialValue?: string) => {
 };
 
 export const number = async (message: string, initialValue?: number) => {
-  const { value } = await enquirer.prompt<{ value: number }>({
+  const { value } = await prompt<{ value: number }>({
     type: "numeral",
     name: "value",
     message,
@@ -29,6 +29,34 @@ export const number = async (message: string, initialValue?: number) => {
   });
   return value ?? 0;
 };
+
+// type SelectOptions =
+//   | {}
+//   | {
+//       type: "select";
+//     }
+//   | {
+//       type: "multiselect";
+//       limit?: number;
+//     }
+//   | {
+//       type: "autocomplete";
+//       footer?: string;
+//       initialIndex?: number;
+//       multiple?: boolean;
+//       suggest?: (value: string) => string[];
+//     };
+// export const select = async (message: string, choices: string[], options: SelectOptions = {}) => {
+//   const { ...otherOptions } = options;
+//   const handler = new Prompt({
+//     ...otherOptions,
+//     type: "type" in options ? options.type : "select",
+//     name: "value",
+//     message,
+//     choices,
+//   });
+//   return handler.run();
+// };
 
 export default {
   confirm,
