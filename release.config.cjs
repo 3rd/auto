62d93387e6bc4180e70b09a7d53b834990b9f538
3rd <3rd@users.noreply.github.com>
2023-05-25 22:11:31 +0300

    chore: configure semantic-release assets

--- release.config.cjs
+++ release.config.cjs
@@ -15,14 +15,14 @@ module.exports = {
         changelogFile: "CHANGELOG.md",
       },
     ],
-    "@semantic-release/npm",
     "@semantic-release/github",
     [
       "@semantic-release/git",
       {
-        assets: ["CHANGELOG.md", "dist/**"],
+        assets: ["CHANGELOG.md", "package.json"],
         message: "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
+    "@semantic-release/npm",
   ],
 };
