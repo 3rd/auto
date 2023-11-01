65f103f94d15d1783a4abe8e2a57881a1557e641
3rd <3rd@users.noreply.github.com>
2023-11-02 00:55:22 +0200

    docs: improve react component story example

--- examples/generate-react-component/__name__.stories.tsx
+++ examples/generate-react-component/__name__.stories.tsx
@@ -1,6 +1,5 @@
 import type { StoryObj, Meta } from "@storybook/react";
 import { __name__ } from "./__name__";
-import type { __name__Props } from "./__name__";
 
 const meta: Meta<typeof __name__> = {
   title: "__name__",
@@ -8,7 +7,7 @@ const meta: Meta<typeof __name__> = {
 };
 export default meta;
 
-type Story = StoryObj<__name__Props>;
+type Story = StoryObj<typeof __name__>;
 
 export const Default: Story = {
   args: {},
