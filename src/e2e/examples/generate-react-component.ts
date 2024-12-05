6d13e1a6807d3e838c9a82ed56a55d9d4567e1e0
3rd <3rd@users.noreply.github.com>
2024-12-05 03:16:46 +0200

    test: fix react test

--- src/e2e/examples/generate-react-component.ts
+++ src/e2e/examples/generate-react-component.ts
@@ -24,10 +24,7 @@ const generateReactComponent: Test = {
   run: (cwd) => {
     return runCommandWithInputs(
       "auto run react-component",
-      [
-        { on: "Component Name", value: "MyComponent" },
-        { on: "Component Path", value: "src/components/MyComponent" },
-      ],
+      [{ on: "Component Path", value: "src/components/MyComponent" }],
       { cwd }
     );
   },
